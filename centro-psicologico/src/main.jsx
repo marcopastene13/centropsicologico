0cf9008597cfb63ce950f7b31b5f1ac878bcc146
Marco Pastene <mpastene1996@outlook.com>
2026-08-12 01:42:26 +0000

    feat: agregar Vercel Analytics

--- centro-psicologico/src/main.jsx
+++ centro-psicologico/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -9,5 +10,6 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
